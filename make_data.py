ac6348fe81ccad342331289201e9f1faab8970e1
Philip Zupancic <philip.zupancic@accenture.com>
2020-08-07 13:21:19 +0200

    more pics

---
+++ make_data.py
@@ -0,0 +1,62 @@
+import numpy
+import matplotlib.pyplot as plt
+import json
+
+timeframe = "old"
+
+if timeframe=="old":
+    jsondata = {
+      "total_tickets": 173,
+      "tickets_by_cat": 
+        {
+          "D00": 13,
+          "D01": 19,
+          "D10": 22,
+          "D11": 1,
+          "D20": 5,
+          "D40": 28,
+          "D43": 14,
+          "D44": 8,
+          "Garbage Bag": 40,
+          "Cardboard": 23
+        },
+      "url1": "https://raw.githubusercontent.com/Phil-Acc/DamageReporter/master/picture2.png",
+      "url2": "https://raw.githubusercontent.com/Phil-Acc/DamageReporter/master/picture1.png",
+      "url3": "https://raw.githubusercontent.com/Phil-Acc/DamageReporter/master/picture0.png",
+      "url4": "https://raw.githubusercontent.com/Phil-Acc/DamageReporter/master/map2.png",
+      "url5": "https://raw.githubusercontent.com/Phil-Acc/DamageReporter/master/map1.png",
+      "url6": "https://raw.githubusercontent.com/Phil-Acc/DamageReporter/master/map0.png"
+    }
+
+else:
+    jsondata = {
+      "total_tickets": 176,
+      "tickets_by_cat": 
+        {
+          "D00": 13,
+          "D01": 19,
+          "D10": 23,
+          "D11": 1,
+          "D20": 6,
+          "D40": 28,
+          "D43": 15,
+          "D44": 8,
+          "Garbage Bag": 40,
+          "Cardboard": 23
+        },
+      "url1": "https://raw.githubusercontent.com/Phil-Acc/DamageReporter/master/picture3.png",
+      "url2": "https://raw.githubusercontent.com/Phil-Acc/DamageReporter/master/picture2.png",
+      "url3": "https://raw.githubusercontent.com/Phil-Acc/DamageReporter/master/picture1.png",
+      "url4": "https://raw.githubusercontent.com/Phil-Acc/DamageReporter/master/map3.png",
+      "url5": "https://raw.githubusercontent.com/Phil-Acc/DamageReporter/master/map2.png",
+      "url6": "https://raw.githubusercontent.com/Phil-Acc/DamageReporter/master/map1.png"
+    }  
+    
+with open('data.json', 'w') as outfile:
+    json.dump(jsondata, outfile)
+    
+plt.barh(list(jsondata["tickets_by_cat"].keys()), list(jsondata["tickets_by_cat"].values()))
+plt.xlabel("Active Cases")
+plt.tight_layout()
+plt.savefig("bardiagram.png", dpi=300)
+plt.show()
